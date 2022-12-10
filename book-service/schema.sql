c94ea05552917d3a2f7f32a206069c4f4f589b8c
nbvannhi <nbvannhi@gmail.com>
2022-12-10 14:02:07 +0700

    Fix mysql syntax errors

--- book-service/schema.sql
+++ book-service/schema.sql
@@ -1,64 +1,46 @@
+DROP DATABASE IF EXISTS book_db;
 CREATE DATABASE book_db;
 USE book_db;
 
 CREATE TABLE publishers (
   publisher_id SERIAL PRIMARY KEY,
   name VARCHAR(20) NOT NULL
-)
+);
 
 CREATE TABLE authors (
   author_id SERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL
-)
+);
 
 CREATE TABLE books (
-  book_id SERIAL,
+  book_id SERIAL PRIMARY KEY,
   isbn13 VARCHAR(13) UNIQUE NOT NULL,
   title VARCHAR(50) NOT NULL,  
   edition VARCHAR(10) NULL,
-  publisher_id INTEGER NOT NULL,
+  publisher_id BIGINT UNSIGNED NOT NULL,
   publication_date DATE NOT NULL,
-  PRIMARY KEY(book_id),
-  CONSTRAINT fk_publisher
-    FOREIGN KEY(publisher_id)
-    REFERENCES publishers(publisher_id),
-  CONSTRAINT fk_series
-    FOREIGN KEY(series_id)
-    REFERENCES series(series_id),
-)
-
-CREATE TYPE series_status AS ENUM (
-  'on_going',
-  'complete'
-)
+  FOREIGN KEY(publisher_id) REFERENCES publishers(publisher_id)
+);
 
 CREATE TABLE series (
-  series_id SERIAL,
-  status series_status,
+  series_id SERIAL PRIMARY KEY,
   length INTEGER NOT NULL,
-  PRIMARY KEY(series_id)
-)
+  status VARCHAR(8),
+  CHECK (status IN ('on_going', 'complete'))
+);
 
 CREATE TABLE book_series (
-  book_id INTEGER NOT NULL,
-  series_id INTEGER NOT NULL,
+  book_id BIGINT UNSIGNED NOT NULL,
+  series_id BIGINT UNSIGNED NOT NULL,
   PRIMARY KEY(book_id, series_id),
-  CONSTRAINT fk_book
-    FOREIGN KEY(book_id)
-    REFERENCES books(book_id) ON DELETE CASCADE,
-  CONSTRAINT fk_series
-    FOREIGN KEY(series_id)
-    REFERENCES series(series_id) ON DELETE CASCADE
-)
+  FOREIGN KEY(book_id) REFERENCES books(book_id) ON DELETE CASCADE,
+  FOREIGN KEY(series_id) REFERENCES series(series_id) ON DELETE CASCADE
+);
 
 CREATE TABLE book_authors (
-  book_id INTEGER NOT NULL,
-  author_id INTEGER NOT NULL,
+  book_id BIGINT UNSIGNED NOT NULL,
+  author_id BIGINT UNSIGNED NOT NULL,
   PRIMARY KEY(book_id, author_id),
-  CONSTRAINT fk_book
-    FOREIGN KEY(book_id)
-    REFERENCES books(book_id) ON DELETE CASCADE,
-  CONSTRAINT fk_author
-    FOREIGN KEY(author_id)
-    REFERENCES authors(author_id) ON DELETE CASCADE
-)
+  FOREIGN KEY(book_id) REFERENCES books(book_id) ON DELETE CASCADE,
+  FOREIGN KEY(author_id) REFERENCES authors(author_id) ON DELETE CASCADE
+);
