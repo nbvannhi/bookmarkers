25eb179e774e7668e5db5ce69a84aff45959bbc4
honganhcs <e0560140@u.nus.edu>
2022-12-14 12:45:43 +0700

    Add a User page that user is directed to after SignIn page

---
+++ frontend/src/pages/user.jsx
@@ -0,0 +1,44 @@
+import SignOutButton from '../components/sign-out-button';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+axios.defaults.withCredentials = true;
+let firstRender = true;
+
+function User() {
+    const [user, setUser] = useState();
+    const refreshToken = async () => {
+        const res = await axios.get('http://localhost:5000/api/refresh', {
+            withCredentials: true,
+        }).catch((err) => console.log(err.response));
+        const data = await res.data;
+        return data;
+    }
+    const sendRequest = async () => {
+        const res = await axios.get('http://localhost:5000/api/user', {
+            withCredentials: true,
+        }).catch((err) => console.log(err.response));
+        const data = await res.data;
+        return data;
+    };
+    useEffect(() => {
+        if (firstRender) {
+            sendRequest().then((data) => setUser(data.user));
+            firstRender = false;
+        } else {
+            let interval = setInterval(
+                () => refreshToken().then((data) => setUser(data.user)),
+                1000 * 29
+            );
+            return () => clearInterval(interval);
+        }
+    }, []);
+    return (
+        <div>
+            {user && <h1>Welcome {user.username}!</h1>}
+            {user && <SignOutButton />}
+        </div>
+    );
+}
+
+export default User;
