1baebc8ea91ad5448e71b7830fd8c2a7af808a3d
Van-Nhi Ba Nguyen <nbvannhi@gmail.com>
2022-12-21 13:21:23 +0700

    Add collection schema

---
+++ collection-service/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS collection_db;
+CREATE DATABASE collection_db;
+USE collection_db;
+
+CREATE TABLE collections (
+  collection_id SERIAL PRIMARY KEY,
+  user_id BIGINT UNSIGNED UNIQUE NOT NULL
+);
+
+CREATE TABLE collection_books (
+  collection_id BIGINT UNSIGNED NOT NULL,
+  book_id BIGINT UNSIGNED NOT NULL,
+  price INTEGER UNSIGNED NULL,
+  note TEXT NULL,
+  PRIMARY KEY(collection_id, book_id),
+  FOREIGN KEY(collection_id) REFERENCES collections(collection_id) ON DELETE CASCADE
+);
