4fdbae23b53433922f689e63ec4e889d09ea89bd
honganhcs <e0560140@u.nus.edu>
2022-12-23 10:25:10 +0700

    Abstract user settings into a separate file

--- frontend/src/components/nav-bar.jsx
+++ frontend/src/components/nav-bar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import UserSettings from './user-settings';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,32 +7,28 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
-const pages = ['Collection', 'Menu2', 'Menu3'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const PUBLIC_PAGES = ['Search', 'Menu2', 'Menu3'];
+const PRIVATE_PAGES = ['Collection', 'Menu2', 'Menu3'];
 
 function NavBar() {
+  const isSignedIn = useSelector((state) => state.isSignedIn) || localStorage.getItem('userId') != null;
+  const pages = isSignedIn ? PRIVATE_PAGES : PUBLIC_PAGES;
+
   const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
-  }
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  }
+  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  }
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  }
+  };
 
   return (
     <AppBar
@@ -129,36 +125,7 @@ function NavBar() {
               </Button>
             ))}
           </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title='Open settings'>
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id='menu-appbar'
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign='center'>{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+          <UserSettings />
         </Toolbar>
       </Container>
     </AppBar>
