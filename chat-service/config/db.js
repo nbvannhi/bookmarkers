45e089fdf3c0b5bdbd5cfd88fd4f5f281d982ca3
honganhcs <e0560140@u.nus.edu>
2022-12-28 10:36:47 +0700

    Set strictQuery to false in user-service and chat-service

--- chat-service/config/db.js
+++ chat-service/config/db.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: true,
     });
   } catch (err) {
     console.error(err.message);
