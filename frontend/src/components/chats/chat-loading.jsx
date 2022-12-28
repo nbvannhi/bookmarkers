9a759b5c7f9897510573e5c9d02f8db11fcf0f94
honganhcs <e0560140@u.nus.edu>
2022-12-28 18:28:27 +0700

    Add ChatLoading component to display when ChatList is loading

---
+++ frontend/src/components/chats/chat-loading.jsx
@@ -0,0 +1,23 @@
+import { Skeleton, Stack } from '@mui/material';
+import React from 'react';
+
+function ChatLoading() {
+  return (
+    <Stack>
+      <Skeleton height='45px' />
+      <Skeleton height='45px' />
+      <Skeleton height='45px' />
+      <Skeleton height='45px' />
+      <Skeleton height='45px' />
+      <Skeleton height='45px' />
+      <Skeleton height='45px' />
+      <Skeleton height='45px' />
+      <Skeleton height='45px' />
+      <Skeleton height='45px' />
+      <Skeleton height='45px' />
+      <Skeleton height='45px' />
+    </Stack>
+  )
+}
+
+export default ChatLoading;
