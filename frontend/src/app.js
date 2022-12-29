45b0000c453732f5bf2fb2c1f7ab8b201cd9092c
honganhcs <e0560140@u.nus.edu>
2022-12-29 23:20:02 +0700

    Add route to ViewAllChats page in app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -7,6 +7,7 @@ import ViewBook from './pages/view-book';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
+import ViewAllChats from './pages/view-all-chats';
 
 function App() {
   const isSignedIn = useSelector((state) => state.isSignedIn) || localStorage.getItem('userId') != null;
@@ -17,9 +18,12 @@ function App() {
         <Routes>
           <Route path='/signin' element={<SignIn />} />
           <Route path='/signup' element={<SignUp />} />
-          {isSignedIn && <Route path='/user' element={<User />} />}{' '}
           <Route path='/books' element={<ViewAllBooks />} />
           <Route path='/books/:book_id' element={<ViewBook />} />
+
+          { /* protected routes*/}
+          {isSignedIn && <Route path='/user' element={<User />} />}{' '}
+          {isSignedIn && <Route path='/chats' element={<ViewAllChats />} />}{' '}
         </Routes>
       </div>
     </React.Fragment>
