4e19f588a1e4f1e49c86a179a2684975bb41cb64
Gaurav Kumar <gk989kumar@gmail.com>
2023-01-17 10:10:33 +0530

    implement validation on email and username

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { SnackbarProvider } from "notistack"
 import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,7 +13,9 @@ root.render(
   <BrowserRouter>
     <React.StrictMode>
       <Provider store={store}>
+      <SnackbarProvider>
         <App />
+        </SnackbarProvider>
       </Provider>
     </React.StrictMode>
   </BrowserRouter>
--- frontend/src/pages/sign-up.jsx
+++ frontend/src/pages/sign-up.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Box, Button, TextField, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from "notistack";
 import axios from 'axios';
 
 function SignUp() {
+   const { enqueueSnackbar } = useSnackbar();
   const history = useNavigate();
   const [inputs, setInputs] = useState({
     email: '',
@@ -25,11 +27,56 @@ function SignUp() {
       password: inputs.password,
     }).catch((err) => console.log(err.response));
   };
+ 
 
   const handleSubmit = (e) => {
+  console.log(inputs);
+   if(validateInput(inputs))
+   {
     e.preventDefault();
+  
     sendRequest().then(() => history('/signin'));
+   }
+   else{
+    if(inputs.username.length===0 )
+    {    
+      enqueueSnackbar("Username is a required field", { variant: `error` });
+    }
+    else if(inputs.username.length<6)
+    {  
+      enqueueSnackbar("Username must be at least 6 characters",  { variant: `error` });
+    }
+    else if(inputs.password.length===0)
+    {
+      enqueueSnackbar("Password is a required field",  { variant: `error` });
+    }
+    else if(inputs.password.length<6)
+    {
+      enqueueSnackbar("Password must be at least 6 characters", { variant: `error` });
+    }
+    else if(inputs.email.length===0)
+    {
+      enqueueSnackbar("Email should not be empty", { variant: `error` });
+    }
+    
+   }
   };
+  const validateInput = (data) => {
+    if(data.username.length===0 ||data.username.length<6)
+      {
+        return false;
+      }
+    else if(data.password.length===0 ||data.password.length<6)
+    {
+      return false;
+    }
+    else if(data.email.length===0)
+    { console.log(data.email,"Asda");
+      return false;
+    }
+    
+  return true;   
+};
 
   return (
     <div>
