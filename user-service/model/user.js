2415599b015e540dc724bc5eaef4f100b125d288
honganhcs <e0560140@u.nus.edu>
2023-01-19 14:44:59 +0800

    Update User model with timestamps

--- user-service/model/user.js
+++ user-service/model/user.js
@@ -2,27 +2,32 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+    },
+    verified: {
+      type: Boolean,
+      required: true,
+      default: false,
+    }
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+  {
+    timestamps: true
   },
-  password: {
-    type: String,
-    required: true,
-    minLength: 8,
-  },
-  verified: {
-    type: Boolean,
-    required: true,
-    default: false,
-  }
-});
+);
 
 module.exports = mongoose.model('User', userSchema);
