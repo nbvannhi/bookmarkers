922718f29607fface4eecc3f50ed1d53e2dba26d
honganhcs <e0560140@u.nus.edu>
2023-01-19 14:45:52 +0800

    Add RequestVerifyEmail page

---
+++ frontend/src/pages/request-verify-email.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+function RequestVerifyEmail() {
+  return (
+    <Box marginTop={10}>
+      <h3>Please check your email for the account verification link! Once verified, you can sign into your account.</h3>
+    </Box>
+  );
+}
+
+export default RequestVerifyEmail;
